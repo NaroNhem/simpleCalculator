961a556cda83e6c87f60d48caf719eb98d7becc1
Justinaro Nhem <n.justinaro@gmail.com>
2022-08-17 16:44:46 -0400

    made the calculator work with numbered pairs

--- script.js
+++ script.js
@@ -28,6 +28,19 @@ const operators = document.querySelectorAll(".operators");
 
 const btnPress = document.querySelectorAll('.button');
 
+const clearBtn = document.querySelector(".clear");
+
+const equals = document.querySelector(".equal");
+
+const aClear = document.querySelector(".aClear");
+
+
+let values = [];
+let opArr = [];
+const backSpace = function () {
+    displayValue.innerHTML = displayValue.innerHTML.substring(0, displayValue.innerHTML.length - 1)
+};
+
 const displayNumber = function (number){
     displayValue.innerHTML += number;
 
@@ -37,15 +50,22 @@ const displayOperator = function(op) {
     displayValue.innerHTML += (" " + op + " ");
     
 }
-const clearBtn = document.querySelector(".clear");
+
 const clear = function () {
     displayValue.innerHTML = "";
 }
 
-operators.forEach(element => {
+const equal = function(array, operator) {
+    for(let i = 0; i < array.length; i ++) {
+
+    }
+}
+
+let opSymbol = operators.forEach(element => {
     element.addEventListener('click', (e) => {
         let value = element.innerHTML;
         displayOperator(value);
+        opArr.push(value);
     })
 })
 
@@ -53,9 +73,37 @@ btnPress.forEach(element => {
     element.addEventListener('click', (event) => {
         let value = parseFloat(element.innerHTML);
         displayNumber(value);
+        console.log(displayValue.innerHTML)
     });
 });
 
 clearBtn.addEventListener('click', (e) => {
+    backSpace();
+    values.pop();
+    opArr.pop();
+})
+
+aClear.addEventListener('click', (e) => {
     clear();
+    opArr = [];
+    values = [];
+})
+
+equals.addEventListener('click', (e) => {
+    let extractNum = displayValue.innerHTML.match(/\d+/g);
+    extractNum.forEach(item => values.push(parseFloat(item)));
+    switch (opArr[0]) {
+        case '+':
+            displayValue.innerHTML = add(values[0],values[1]);
+            break;
+        case '-':
+            displayValue.innerHTML = subtract(values[0],values[1]);
+            break;
+        case '/':
+            displayValue.innerHTML = divide(values[0], values[1]);
+            break;
+        case '*':
+            displayValue.innerHTML = multiply(values[0],values[1]);
+            break;
+    }
 })
